2f170e1308032699869c4bad7b6a8922fdd59e91
kker <907765631@qq.com>
2020-11-22 14:18:13 +0800

    [Mamingjun] feat: implement docker

---
+++ alice/src/main/java/com/thoughtworks/capability/gtb/alice/AliceApi.java
@@ -0,0 +1,22 @@
+package com.thoughtworks.capability.gtb.alice;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class AliceApi {
+
+    public final String BOB_URL = "http://bob:8081";
+
+    @GetMapping("/hello")
+    @ResponseStatus(HttpStatus.OK)
+    String getHelloFromBob() {
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> response = restTemplate.getForEntity(BOB_URL + "/hello", String.class);
+        return response.getBody();
+    }
+}
---
+++ bob/src/main/java/com/thoughtworks/capability/gtb/bob/BobApi.java
@@ -0,0 +1,17 @@
+package com.thoughtworks.capability.gtb.bob;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BobApi {
+
+    @GetMapping("/hello")
+    @ResponseStatus(HttpStatus.OK)
+    String getHelloFromBob() {
+        return "Hello from Bob!";
+    }
+
+}
